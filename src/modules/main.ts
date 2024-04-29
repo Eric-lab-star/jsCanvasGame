28b78e59355edd7ca3e8aba4531ea61994613f03
kimkyungsub <cyon256@icloud.com>
2024-04-29 09:40:59 +0900

    hi

--- src/modules/main.ts
+++ src/modules/main.ts
@@ -2,6 +2,8 @@ import Game from "./game.js";
 
 // main();
 function main() {
+  console.log("hello");
+
   const canvas = document.querySelector("canvas");
 
   canvas.width = innerWidth;
@@ -9,7 +11,6 @@ function main() {
 
   const ctx = canvas.getContext("2d");
   const game = new Game(ctx);
-
   game.start();
 }
 

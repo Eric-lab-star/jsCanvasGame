eacc46d2e5128ffbeee3ee3cf8a87782b6bf3ef0
kimkyungsub <cyon256@icloud.com>
2024-05-14 23:59:27 +0900

    jsom

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -1,11 +1,25 @@
 import { expect, describe, it, vi } from "vitest";
 import { setCanvasWidthHeight } from "../main/main";
+import jsdom from "jsdom";
+const { JSDOM } = jsdom;
+const {
+  window: { document },
+} = new JSDOM(
+  `
+<!doctype html>
+<html lang="en">
+    <canvas></canvas>
+  </body>
+</html> `,
+  { pretendToBeVisual: true },
+);
 
 describe("setCanvasWidthHeight()", () => {
   it("set canvas width and height", () => {
     const canvas = document.querySelector("canvas");
     if (canvas != null) {
       setCanvasWidthHeight(canvas);
+      expect(canvas.height).toBe(innerHeight);
       expect(canvas.width).toBe(innerWidth);
     }
   });

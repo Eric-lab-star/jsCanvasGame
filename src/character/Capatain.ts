031731193a05cf130a77510a976f97e479dbf21f
kimkyungsub <cyon256@icloud.com>
2024-05-22 04:50:01 +0900

    add Captain input listeners

--- src/character/Capatain.ts
+++ src/character/Capatain.ts
@@ -1,7 +1,13 @@
+import KeyBoardInput from "../inputs/Keyboard";
 import Vector2d from "../utilz/Vector2d";
 import Character from "./Character";
 
 export default class Captain extends Character {
+  private left: boolean = false;
+  private right: boolean = false;
+  private up: boolean = false;
+  private down: boolean = false;
+
   constructor(ctx: CanvasRenderingContext2D, position: Vector2d) {
     super(
       ctx,
@@ -11,6 +17,66 @@ export default class Captain extends Character {
       playerStates,
       captainImg,
     );
+    this.addKeyListener(new KeyBoardInput(this));
+  }
+
+  private addKeyListener(input: KeyBoardInput) {
+    addEventListener("keydown", (event: KeyboardEvent) => {
+      input.keyPressed(event.key);
+      this.moveCaptain();
+    });
+
+    addEventListener("keyup", (event: KeyboardEvent) => {
+      input.keyReleased(event.key);
+      this.moveCaptain();
+    });
+  }
+
+  private moveCaptain() {
+    if (this.isRight()) {
+      this.pos.update(this.speed, 0);
+    }
+    if (this.isLeft()) {
+      this.pos.update(-this.speed, 0);
+    }
+    if (this.isUp()) {
+      this.pos.update(0, -this.speed);
+    }
+    if (this.isDown()) {
+      this.pos.update(0, this.speed);
+    }
+  }
+
+  public setLeft(bool: boolean) {
+    this.left = bool;
+  }
+
+  public isLeft() {
+    return this.left;
+  }
+
+  public setRight(bool: boolean) {
+    this.right = bool;
+  }
+
+  public isRight() {
+    return this.right;
+  }
+
+  public setUp(bool: boolean) {
+    this.up = bool;
+  }
+
+  public isUp() {
+    return this.up;
+  }
+
+  public setDown(bool: boolean) {
+    this.down = bool;
+  }
+
+  public isDown() {
+    return this.down;
   }
 }
 
--- src/inputs/Keyboard.ts
+++ src/inputs/Keyboard.ts
@@ -1,24 +1,62 @@
-import Character from "../character/Character";
+import Captain from "../character/Capatain";
 
-class KeyBoardInput {
-  protected component: Character;
-  constructor(component: Character) {
-    this.component = component;
-    addEventListener("keydown", (event) => this.move(event));
+export default class KeyBoardInput {
+  protected character: Captain;
+  constructor(character: Captain) {
+    this.character = character;
   }
 
-  move(event: KeyboardEvent) {
-    switch (event.key) {
-      case "d" || "D":
-        this.component.update(this.component.speed, 0);
+  public keyPressed(key: string) {
+    switch (key) {
+      case "D":
+      case "ㅇ":
+      case "d":
+        this.character.setRight(true);
         break;
-      case "a" || "A":
-        this.component.update(-this.component.speed, 0);
+      case "a":
+      case "A":
+      case "ㅁ":
+        this.character.setLeft(true);
+        break;
+      case "w":
+      case "W":
+      case "ㅈ":
+        this.character.setUp(true);
+        break;
+      case "s":
+      case "S":
+      case "ㄴ":
+        this.character.setDown(true);
+        break;
+      default:
+        break;
+    }
+  }
+
+  public keyReleased(key: string) {
+    switch (key) {
+      case "D":
+      case "ㅇ":
+      case "d":
+        this.character.setRight(false);
+        break;
+      case "a":
+      case "A":
+      case "ㅁ":
+        this.character.setLeft(false);
+        break;
+      case "w":
+      case "W":
+      case "ㅈ":
+        this.character.setUp(false);
+        break;
+      case "s":
+      case "S":
+      case "ㄴ":
+        this.character.setDown(false);
         break;
       default:
         break;
     }
   }
 }
-
-export default KeyBoardInput;

5c95e083b8a12f1e233ff5675903726c7c83cd40
kimkyungsub <cyon256@icloud.com>
2024-06-07 14:04:04 +0900

    rafactor tilemap

--- src/main/tileMap.ts
+++ src/main/tileMap.ts
@@ -1,26 +1,6 @@
 export default class TileMapProps {
-  private static MAP_COLUMNS: number = 40;
-  private static MAP_ROWS: number = 25;
-  private static TILE_SIZE: number = 32;
-  private static TILES_MAP_COLUMNS: number = 17;
-
-  constructor() {}
-  public getTileSize() {
-    return TileMapProps.TILE_SIZE;
-  }
-
-  public getTilesMapColumns() {
-    return TileMapProps.TILES_MAP_COLUMNS;
-  }
-
-  public getMapColumns() {
-    return TileMapProps.MAP_COLUMNS;
-  }
-
-  public getMapRows() {
-    return TileMapProps.MAP_ROWS;
-  }
-  /**
-   *
-   */
+  public static MAP_COLUMNS: number = 40;
+  public static MAP_ROWS: number = 25;
+  public static TILE_SIZE: number = 32;
+  public static TILES_MAP_COLUMNS: number = 17;
 }

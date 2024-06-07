e4b27e71bb1e339855db3f235e6973cd9af2b572
kimkyungsub <cyon256@icloud.com>
2024-06-07 20:05:32 +0900

    refactor CanvasEnv

--- src/env/CanvasEnv.ts
+++ src/env/CanvasEnv.ts
@@ -1,26 +1,24 @@
 export default class CanvasEnv {
   public canvas: HTMLCanvasElement;
-  public ctx: CanvasRenderingContext2D;
   private static zindex: number = 0;
 
   public constructor(width: number, height: number) {
     this.canvas = document.createElement("canvas");
-    this.ctx = this.canvas.getContext("2d")!;
     this.setCanvasSize(width, height);
+    this.setCanvasStyle();
     document.body.appendChild(this.canvas);
   }
 
-  public setCanvasSize(width: number, height: number) {
+  private setCanvasSize(width: number, height: number) {
     this.canvas.width = width;
     this.canvas.height = height;
   }
 
-  public setCanvasStyle() {
+  private setCanvasStyle() {
     this.canvas.style.position = "absolute";
     this.canvas.style.left = "0";
     this.canvas.style.top = "0";
     this.canvas.style.zIndex = CanvasEnv.zindex.toString();
     CanvasEnv.zindex++;
-    console.log(CanvasEnv.zindex);
   }
 }
